e9f870a60013b248f7d0b953279720799261e177
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2022-04-01 09:57:51 +0200

    Update gemmested.py

--- gemmested.py
+++ gemmested.py
@@ -19,5 +19,17 @@ Graph = namedtuple("Graph",["nodes","edges"])
 
 g = Graph([i + 1 for i in range(N)], edges)
 
+#a binary search implementation
+def binarySearch(arr, target, l, r):
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
+
 
 
